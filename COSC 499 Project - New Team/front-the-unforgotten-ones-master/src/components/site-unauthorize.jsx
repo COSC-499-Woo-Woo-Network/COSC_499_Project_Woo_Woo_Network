1a9bdd891f71a67ba4d0c9e37501041ef11af3d2
owenspicker <71356125+owenspicker@users.noreply.github.com>
2021-11-22 21:05:57 -0800

    added test files for displaying location

--- COSC 499 Project - New Team/front-the-unforgotten-ones-master/src/components/site-unauthorize.jsx
+++ COSC 499 Project - New Team/front-the-unforgotten-ones-master/src/components/site-unauthorize.jsx
@@ -12,6 +12,7 @@ import AboutUs from './about-us';
 import PrivacyPolicy from './privacy-policy';
 import jwt_decode from 'jwt-decode';
 import SignUp2 from './admin-account-search.jsx';
+import testFile from './testFile';
 /**  This component renders when you are not logged in. It has the login and signup pages,
   while the logged-in version would not have these. You can still look at healer pages
   while not logged in.
@@ -31,6 +32,7 @@ const NoAuthSite = (props) => {
           <Route exact path="/login" component={LoginPage}></Route>
           <Route exact path="/signup" component={SignUp}></Route>
           <Route exact path="/account-search" component={SignUp2}></Route>
+          <Route exact path="/test/:id" component={testFile}></Route>
           <Route
             exact
             path="/forgot-password"
